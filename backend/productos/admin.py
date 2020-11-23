bfa17aeddaf31f54735b7faf0722aa9aca7ec921
Espita <ebuitragod@gmail.com>
2020-11-23 15:22:18 -0500

    anadiendo features en admin.py para subir csvs.

--- backend/productos/admin.py
+++ backend/productos/admin.py
@@ -83,4 +83,56 @@ class Category(admin.ModelAdmin):
     #list_filter = ()
     search_fields = ['code', 'name']
     
+@admin.register(Consumption)
+class ConsumptionAdmin(admin.ModelAdmin):
 
+    change_list_template = 'admin_productos/consumption_changelist.html'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls[
+            path('import-csv/', self.import_csv),
+        ]
+        return my_urls + urls
+    
+    def import_csv(self, request):
+        if request.method == 'POST':
+            csv_file = request.FILES['csv_file']
+            data_set = pd.read_csv(io.StringIO(csv_file.read().decode('UTF-8')), sep=',')
+            data_set = data_set.fillna(0)
+            data_set['timestamp'] = data_set['timestamp'].astype(int)
+            data_set['product'] = data_set['product'].astype(str)
+            data_set['quantity'] = data_set['quantity'].astype(int)
+
+            for index, row in data_set.iterrows():
+                Consumption.objects.update_or_create(
+                    timestamp = datetime.fromtimestamp(row['timestamp']),
+                    product = row['product'],
+                    quantity = row['quantity']
+                )
+            
+            self.message_user(request, 'El seu csv ha estat importat.')
+            return redirect('..')
+
+        form = CsvImportForm()
+        payload = {'form': form}
+        return render(
+            request, 'admin_productos/csv_form.html', payload
+        )
+
+    fieldsets = [
+        (
+            None,           {'fields':[
+                                        'timestamp',
+                                        'product',
+                                        'quantity',
+                                      ]
+                            }
+        )
+    ]
+
+    list_display = ('product', 'timestamp', 'quantity')
+    #list_filter = ('product', 'timestamp', 'quantity')
+    search_fields = ('product', 'timestamp')
+
+    
--- backend/productos/models/category.py
+++ backend/productos/models/category.py
@@ -6,8 +6,14 @@ from django.utils.timezone import now
 
 
 class Category(models.Model):
-    code = models.CharField('Código', max_length=10, blank=False, null=False)
-    name = models.CharField('Nombre de la categoría', max_length=100, blank=False, null=False,)
+    CODE = [
+        ('LL': 'LL - Landline'),
+        ('MO', 'MO - mobile'),
+    ]
+
+    code = models.CharField('Código', max_length=2, blank=False, null=False)
+    
+    # name = models.CharField('Nombre de la categoría', max_length=100, blank=False, null=False,)
 
     def __str__(self):
         return '({}): {}'.format(self.code, self.name)
